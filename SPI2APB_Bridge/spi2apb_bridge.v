d355ad6c2a673faf836058b4654623badfc92345
Artur <100425493+jacky2256@users.noreply.github.com>
2022-03-01 16:03:30 +0300

    added gitignore

--- SPI2APB_Bridge/spi2apb_bridge.v
+++ SPI2APB_Bridge/spi2apb_bridge.v
@@ -1,4 +1,20 @@
-module spi2apb_bridge(input );
+module spi2apb_bridge(
+                        input 			sclk,
+                        input 			resetn,
+                        input 			mosi,
+                        input 	[2:0]	ss,
+                        output 			miso,
+
+                        input			b_prdata,
+                        input			b_pready,
+                        output			b_pclk,
+                        output			b_resetn,
+                        output			b_pwdata,
+                        output			b_pwrite,
+                        output			b_psel,
+                        output			b_penable,
+                        output			b_paddr
+                        );
 
 
 
